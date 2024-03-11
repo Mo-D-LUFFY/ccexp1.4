87f31aa110befb2860b33933d7542cf8488d210d
Ayush Raj <95172843+Mo-D-LUFFY@users.noreply.github.com>
2024-03-11 11:10:37 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
-print("Ayush Raj CC exp 1.4")
+print("Ayush Raj CC Lab Mst")
+name=input(print("Enter your Name"))
+  
+email=input(print("Enter your Email Id"))
+phone=input(print("Enter your Phone Number"))
+cgpa=input(print("Enter your current cgpa"))
+print("Enter your 3 Hobbies")
+lst =[]
+for i in range(3):
+  hobby=input()
+  lst.append(hobby)
+print("Enter 3 Project Names")
+pro = []
+for i in range(3):
+  proj=input()
+  pro.append(proj)
+company=input(print("Enter company to apply"))
+print("...............RESUME................")
+print(name)
+print("I "+ name+ " want to join your comapny "+ company+ " for SDE position")
+print("My Conatact info "+ phone+ " , "+email)
+print("Current CGPA : " +cgpa )
+print("My Hobbies: "+lst)
+print("My projects: "+pro)
+
+
